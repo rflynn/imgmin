7894ed6dbb6650f56654a9a29caf1b3c0fc589af
Ryan Flynn <parseerror@gmail.com>
2011-06-10 17:38:23 -0400

    refactor, add spinner

--- web/index.php
+++ web/index.php
@@ -7,10 +7,23 @@ body { font-family: arial; font-size:small }
 .err { font-size:200%; color: red }
 .dir { font-family: Courier New, sans-serif }
 code { color: green }
+h3 { margin: 0px; margin-bottom: 3px }
+#thumbs { width:280px }
 </style>
 </head>
 <body>
+<div id="spinner" style="background-color:#fff; position:absolute"><img src="static/img/spinner.gif"></div>
 <script language="javascript" src="static/js/jquery-1.6.1.js"></script>
+<script>
+jQuery.fn.center = function () {
+	this.css("position","absolute");
+	this.css("top", ( $(window).height() - this.height() ) / 2+$(window).scrollTop() + "px");
+	this.css("left", ( $(window).height() - this.width() ) / 2+$(window).scrollLeft() + "px");
+	return this;
+}
+$('#spinner').center();
+$('#spinner').show();
+</script>
 
 <?php
 
@@ -25,9 +38,12 @@ code { color: green }
  *	pngnq http://pngnq.sourceforge.net/
  */
 
-define('THUMBSIZE', 125);
+define('THUMBSIZE', 100);
 define('TEMPDIR', './tmp/');
 define('TEMPABS', realpath('.') . '/' . TEMPDIR);
+define('IMGMINPATH', './');
+#define('IMAGEMAGICKPATH', '/home/rflynn/ImageMagick-6.7.0-6/utilities/');
+define('IMAGEMAGICKPATH', '/usr/local/bin/');
 
 // check temp dir setup
 if (!file_exists(TEMPDIR))
@@ -49,7 +65,7 @@ function displayThumb($orig)
 		$pct = round($res->bytes / $orig->bytes * 100);
 		$sizediff = $orig->bytes - $res->bytes;
 		$cmp = $res->cmp($orig) . '%';
-		preg_match('/(\d\.\d\d).(jpe?g|gif|png)$/i', $res->path, $m);
+		preg_match('/(\d{1,2}\.\d\d).(jpe?g|gif|png)$/i', $res->path, $m);
 		$target_diff = @$m[1] ? $m[1] : '0';
 ?>
 $('#thumbs').html($('#thumbs').html() +
@@ -57,7 +73,7 @@ $('#thumbs').html($('#thumbs').html() +
 '	<div id="row<?= $i ?>" class="row" style="display:table-row">' +
 '		<div style="display:table-cell; width:<?= THUMBSIZE ?>px"><a href="javascript:showThumb(\'row<?= $i ?>\', \'<?= $res->path ?>\')"><img src="<?= $res->thumbpath ?>" height="<?= $orig->thumbheight ?>" width="<?= $orig->thumbwidth ?>" style="border-width:1px"></a></div>' +
 '		<div style="display:table-cell; text-align:left; vertical-align:top; padding:2px; border:1px solid #ccc">' +
-'			<?= $target_diff ?><span style="color:#999;font-size:110%">σ</span><br>' +
+'			<b><?= $target_diff ?><span style="color:#999;font-size:110%">&sigma;</span></b><br>' +
 '			<div style="display:inline-block; margin-right:3px; width:<?= $pct ?>px; height:10px; background-image:url(static/img/progress-bar.png)"></div><?= $pct ?>%<br>' +
 '			Size: <?= round($res->bytes / 1024.0, 1) ?> Kb<br>' +
 '			Saved: <?= round($sizediff / 1024.0, 1) ?> Kb' +
@@ -92,8 +108,14 @@ class Img
 	{
 		$thumbpath = $this->tmpdir . basename($this->path) . '.thumb.' . $this->ext;
 		if (!file_exists($thumbpath))
-			exec(sprintf('convert -resize %ux%u %s %s',
-				$this->thumbpx, $this->thumbpx, escapeshellarg($this->path), escapeshellarg($thumbpath)));
+		{
+			$cmd = sprintf('%sconvert -resize %ux%u %s %s 2>&1',
+				IMAGEMAGICKPATH, $this->thumbpx, $this->thumbpx, escapeshellarg($this->path), escapeshellarg($thumbpath));
+			#echo "<pre>$cmd</pre>";
+			exec($cmd, &$out, &$err);
+			#echo '<pre>'.print_r($out,1).'</pre>';
+			#print_r($err);
+		}
 		return $thumbpath;
 	}
 
@@ -103,7 +125,11 @@ class Img
 		/*
 		 * -metric: algorithm for comparison. SSIM > RMSE, but compare doesn't support it.
 		 */
-		$sys = shell_exec(sprintf('compare -metric RMSE %s %s /dev/null 2>&1', escapeshellarg($this->path), escapeshellarg($other->path)));
+		$cmd = sprintf('%scompare -metric RMSE %s %s /dev/null 2>&1',
+			IMAGEMAGICKPATH, escapeshellarg($this->path), escapeshellarg($other->path));
+		#echo "<pre>$cmd</pre>";
+		$sys = shell_exec($cmd);
+		#echo "<pre>$sys</pre>";
 		// extract pct difference
 		if (preg_match('/\((.*?)\)/', $sys, $m))
 			$cmp = round(floatval($m[1]) * 100, 2);
@@ -125,11 +151,16 @@ class Img
 
 	function resampleJPG()
 	{
-		foreach (array(0.5, 0.75, 1, 1.5, 2, 3) as $pct)
+		foreach (array(0.5, 0.75, 1, 1.5, 2, 3, 4, 10) as $pct)
 		{
 			$newpath = sprintf('%s%s.%4.2f.%s', $this->tmpdir, basename($this->path), $pct, $this->ext);
 			if (!file_exists($newpath))
-				exec(sprintf('../imgmin.sh %s %s %.2f', escapeshellarg($this->path), escapeshellarg($newpath), $pct));
+			{
+				$cmd = sprintf('%simgmin.sh %s %s %.2f', IMGMINPATH, escapeshellarg($this->path), escapeshellarg($newpath), $pct);
+				#echo "<pre>$cmd</pre>";
+				exec($cmd, &$out);
+				#echo '<pre>'.print_r($out,1).'</pre>';
+			}
 			$this->resampled[] = new Img($newpath);
 		}
 	}
@@ -164,7 +195,7 @@ function safedir($dir)
 	return $safer;
 }
 
-function list_images($dir)
+function list_images($dir, $nth=null)
 {
 	$files = array();
 	$d = opendir($dir);
@@ -172,46 +203,98 @@ function list_images($dir)
 		if (preg_match('/\.(jpe?g|gif|png)$/i', $f))
 			$files[] = "$dir/$f";
 	closedir($d);
+	if ($nth !== null)
+		return $files[$nth];
 	return $files;
 }
 
 $DIR = @$_GET['dir'] ? safedir($_GET['dir']) : 'images';
-$IMG = @$_GET['img'] ? $_GET['img'] : '1hulk-554x398.jpg';
+$IMG = @$_GET['img'] ? $_GET['img'] : list_images($DIR, 0);
 $old = new Img($IMG);
-
 ?>
 
-<div>
-<?php foreach (list_images($DIR) as $f) {
-	$img = new Img($f);
+<h1>imgmin demo</h1>
+
+<div style="display:table">
+<div class="r">
+
+<?php
+$classify = array(
+	array('Small',      200, array()),
+	array('Medium',	    400, array()),
+	array('Large',	    1e6, array()),
+);
+# partition images
+$li = list_images($DIR);
+asort($li);
+foreach ($li as $img)
+{
+	list($width, $height, $type, $attr) = getimagesize($img);
+	for ($i = 0; $i < count($classify); $i++)
+	{
+		if (max($width, $height) <= $classify[$i][1])
+		{
+			$classify[$i][2][] = $img;
+			break;
+		}
+	}
+}
+
+foreach ($classify as $c)
+{
+	list($title, $size, $images) = $c;
+	if ($images)
+	{
 ?>
-	<div style="display:inline-block"><a href="<?= $PHP_SELF ?>?dir=<?= $DIR ?>&img=<?= $img->path ?>"><img src="<?= $img->thumbpath ?>" width="<?= $img->thumbwidth ?>" height="<?= $img->thumbheight ?>" style="border-width:1px"></a></div>
-<?php }
-ob_flush();
+	<div class="c" style="vertical-align:top; width:<?= THUMBSIZE ?>px; padding:1px; border:1px solid #ccc; background-color:#eee">
+		<h3><?= $title ?></h3>
+<?php
+	foreach ($images as $f)
+	{
+		$img = new Img($f);
+		$link = $PHP_SELF . '?img=' . urlencode($img->path);
+		if ($DIR != 'images')
+			$link .= 'dir='.urlencode($DIR);
 ?>
-</div>
+		<div style="display:inline-block; padding:0px; margin:0px; margin-right:1px; margin-bottom:1px"><a href="<?= $link ?>"><img src="<?= $img->thumbpath ?>" width="<?= $img->thumbwidth ?>" height="<?= $img->thumbheight ?>" style="border-width:1px; padding:0px; margin:0px"></a></div>
+<?php
+	}
+?>
+	</div>
+<?php
+	}
+}
+?>
+
+
+<div class="c">
 
 <div style="display:table">
 	<div class="r">
-		<div class="c"><em>Original</em></div>
-		<div class="c"><em>Resampled</em></div>
-		<div class="c"><em>Sample Gallery</em></div>
 	</div>
 	<div class="r">
-		<div class="c">
-			<div style="background-image:url(static/img/bg.png)" >
-				<img id="oldimg" src="<?= htmlentities($old->path) ?>" width="<?= $old->width ?>" height="<?= $old->height ?>">
-			</div>
+		<div class="c" style="vertical-align:top; padding-left:10px">
+			<div class="c"><h3>Sample Gallery</h3></div>
+			<div id="thumbs"></div>
 		</div>
-		<div class="c">
+		<div id="resampled" class="c" style="min-width:50%">
+			<div><h3>Resampled</h3></div>
 			<div style="background-image:url(static/img/bg.png)" >
-				<img id="newoldbg" src="<?php echo htmlentities($old->path) ?>" style="position:absolute; z-index:0" width="<?= $old->width ?>" height="<?= $old->height ?>">
+				<img id="newoldbg" src="<?= htmlentities($old->path) ?>" style="position:absolute; z-index:0" width="<?= $old->width ?>" height="<?= $old->height ?>">
 				<img id="newimg" src="" width="<?= $old->width ?>" height="<?= $old->height ?>" style="z-index:1">
 			</div>
+			<div>
+				<div><h3>Original</h3></div>
+				<div style="background-image:url(static/img/bg.png)" >
+					<img id="oldimg" src="<?= htmlentities($old->path) ?>" width="<?= $old->width ?>" height="<?= $old->height ?>">
+				</div>
+			</div>
 		</div>
-		<div id="thumbs" class="c" style="vertical-align:top; padding-left:1px"></div>
 	</div>
 </div>
+
+</div> <!-- row -->
+</div> <!-- table -->
 
 <script language="javascript">
 
@@ -221,7 +304,7 @@ function showThumb(a, path)
 	$('#newoldbg').show()
 	$('#newimg').hide()
 	$('#newimg').attr('src', path)
-	$('#newimg').fadeIn('fast', function()
+	$('#newimg').fadeIn('slow', function()
 	{
 		$('#newoldbg').hide()
 	})
@@ -229,15 +312,18 @@ function showThumb(a, path)
 	$('#' + a).css('background-color', '#eee')
 }
 
+
 $(document).ready(function() {
 
 <?php
+ob_flush();
 $old->resample();
 displayThumb($old);
 ?>
 
-
 })
+
+$('#spinner').hide();
 
 </script>
 </body>
@@ -266,7 +352,7 @@ function show_cmds($mkdir=false)
 	}
 ?>
 		<div><code>sudo chgrp <?= $www_group ?> <?= TEMPABS ?> # change group</code></div>
-		<div><code>sudo chmod g+w <?= $www_group ?> <?= TEMPABS ?> # grant writes to group</code></div>
+		<div><code>sudo chmod g+w <?= TEMPABS ?> # grant writes to group</code></div>
 	</div>
 <?php
 }
@@ -282,3 +368,4 @@ function tempdir_error($msg, $mkdir=false)
 }
 
 ?>
+
