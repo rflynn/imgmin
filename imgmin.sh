9f6f96fc58c5492e169c34ecfc07769c6d26d1b2
Ryan Flynn <parseerror@gmail.com>
2011-06-20 15:06:21 -0400

    avoid resampling images with too low color density; images with low color density tend to pixelate badly.

--- imgmin.sh
+++ imgmin.sh
@@ -37,8 +37,19 @@ if [ ! -f $src ]; then
 	exit 1
 fi
 
+# if file isn't color-dense enough it'll get badly pixelated
+# most of the photos i'm working on now have a value of 1-10
+INV_COLOR_DENSITY_THRESHOLD=50
+inv_color_density=`convert "$src" -format "%h*%w/%k" info:- | bc -l`
+if [ `echo "$inv_color_density > $INV_COLOR_DENSITY_THRESHOLD" | bc` -eq 1 ]; then
+	echo "Color density too low, using original."
+	cp $src $dst
+	exit 0
+fi
+
 ext=${src##*.}
 tmpfile=/tmp/imgmin$$.jpg
+
 
 qmin=0
 qmax=100
@@ -63,9 +74,10 @@ kdiff=$((($k0-$k1) * 100 / $k0))
 
 if [ $kdiff -gt 0 ]; then
 	mv $tmpfile $dst
-	echo "Before:${k0}KB After:${k1}KB Saved:$((k0-k1))KB($kdiff%)"
 else
 	cp $src $dst
-	echo "Couldn't minimize within $cmpthreshold, using original"
+	k1=$k0
+	kdiff=0
 fi
+echo "Before:${k0}KB After:${k1}KB Saved:$((k0-k1))KB($kdiff%)"
 
