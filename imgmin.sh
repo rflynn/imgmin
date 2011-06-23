fa1c12ec1752756261bb9791ba1a183a24b0ac80
Ryan Flynn <parseerror@gmail.com>
2011-06-23 12:34:40 -0400

    broken; most of the time spent porting shell scripts is consumed by fighting with them

--- imgmin.sh
+++ imgmin.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Image minimizer
 # Iteratively resamples image quality to a certain threshold, reducing image filesize but retaining quality similar to the original image
 #
 # Example usage:
-#	./imgmin.sh foo.jpg foo-min1.jpg	# default 1 standard deviation
-#	./imgmin.sh foo.jpg foo-min2.jpg 2	# quality within 2 standard deviation
+#	./imgmin.sh foo-before.jpg foo-after.jpg
 #
 # Author: Ryan Flynn <parseerror+imgmin@gmail.com>
 #
@@ -25,56 +24,74 @@ fi
 # needed for webservers
 PATH=$PATH:/usr/local/bin:/usr/bin # FIXME: be smarter
 
+CMP_THRESHOLD=0.90
+COLOR_DENSITY_RATIO=0.95
+MIN_UNIQUE_COLORS=4096
+
 src=$1
 dst=$2
-cmpthreshold=${3:-1}
 
 if [ ! -f $src ]; then
 	echo "File $src does not exist"
 	exit 1
 fi
 
-# if file isn't color-dense enough it'll get badly pixelated
-# most of the photos i'm working on now have a value of 1-10
-INV_COLOR_DENSITY_THRESHOLD=50
-inv_color_density=`convert "$src" -format "%h*%w/%k" info:- | bc -l`
-if [ `echo "$inv_color_density > $INV_COLOR_DENSITY_THRESHOLD" | bc` -eq 1 ]; then
-	echo "Color density too low, using original."
-	cp $src $dst
-	exit 0
-fi
+function unique_colors
+{
+	src=$1
+	colors=`convert "$src" -format "%k" info:-`
+	return $colors
+}
 
-ext=${src##*.}
-tmpfile=/tmp/imgmin$$.jpg
-
-
-qmin=0
-qmax=100
-# binary search for lowest quality where compare < $cmpthreshold
-while [ $qmax -gt $((qmin+1)) ]
-do
-	q=$(((qmax+qmin)/2))
-	convert -quality $q -interlace Line -strip $src $tmpfile
-	cmppct=`compare -metric RMSE $src $tmpfile /dev/null 2>&1 | cut -d '(' -f2 | cut -d ')' -f1`
-	cmppct=`echo $cmppct*100 | bc`
-	if [ `echo "$cmppct > $cmpthreshold" | bc` -eq 1 ]; then
-		qmin=$q
-	else
-		qmax=$q
+function search_quality
+{
+	src=$1
+	tmpfile=$1
+	uc=`unique_colors "$src"`
+	echo "uc=$uc"
+	if [ $((uc < MIN_UNIQUE_COLORS)) ]; then
+		#return
+		echo "$uc < $MIN_UNIQUE_COLORS"
 	fi
-	printf "%.2f@%u " $cmppct $q
-done
+	qmin=0
+	qmax=100
+	# binary search for lowest quality where compare < $cmpthreshold
+	while [ $qmax -gt $((qmin+1)) ]
+	do
+		q=$(((qmax+qmin)/2))
+		convert -quality $q $src $tmpfile
+		cmppct=`compare -metric RMSE $src $tmpfile /dev/null 2>&1 | cut -d '(' -f2 | cut -d ')' -f1`
+		cmppct=`echo $cmppct*100 | bc`
+		if [ `echo "$cmppct > $cmpthreshold" | bc` -eq 1 ]; then
+			qmin=$q
+		else
+			qmax=$q
+		fi
+		printf "%.2f@%u " $cmppct $q
+	done
+}
 
-k0=$((`stat -c %s $src` / 1024))
-k1=$((`stat -c %s $tmpfile` / 1024))
-kdiff=$((($k0-$k1) * 100 / $k0))
+function print_stats
+{
+	k0=$((`stat -c %s $src` / 1024))
+	k1=$((`stat -c %s $tmpfile` / 1024))
+	kdiff=$((($k0-$k1) * 100 / $k0))
+	if [ $kdiff -eq 0 ]; then
+		k1=$k0
+		kdiff=0
+	fi
+	echo "Before:${k0}KB After:${k1}KB Saved:$((k0-k1))KB($kdiff%)"
+	return $kdiff
+}
 
-if [ $kdiff -gt 0 ]; then
-	mv $tmpfile $dst
-else
-	cp $src $dst
-	k1=$k0
-	kdiff=0
-fi
-echo "Before:${k0}KB After:${k1}KB Saved:$((k0-k1))KB($kdiff%)"
+color_cnt=`unique_colors $src`
+pixel_count=$((`convert "$src" -format "%w*%h" info:-`))
+original_density=$((color_cnt / pixel_count))
+
+tmpfile=/tmp/imgmin$$.jpg
+search_quality $src $tmpfile
+convert -strip $tmpfile $dst
+kdiff=print_stats
+cp $tmpfile $dst
+#rm -f $tmpfile
 
