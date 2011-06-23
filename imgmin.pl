5513a2741a8ea5f6aba487f9d37ff5c8e8dd2b09
Ryan Flynn <parseerror@gmail.com>
2011-06-23 16:29:33 -0400

    Organize output into Before/During/After, comment threshold constants

--- imgmin.pl
+++ imgmin.pl
@@ -6,20 +6,29 @@
 # PerlMagick: http://www.imagemagick.org/script/perl-magick.php
 #
 
+#
+# typically images contain more information than humans need to view it.
+# the goal of this program is to reduce image file size by reducing quality
+# while not visibly affecting image quality for casual use.
+#
+
 # TODO: consider incorporating: http://en.wikipedia.org/wiki/Color_vision#Physiology_of_color_perception
 
 use strict;
 use Image::Magick;
 use File::Copy qw(copy);
-use List::Util qw(max);
+use List::Util qw(max min);
 
 $|++;
 
-use constant CMP_THRESHOLD		=>    0.90;
-use constant COLOR_DENSITY_RATIO	=>    0.95; 
-use constant MIN_UNIQUE_COLORS		=> 4096; 
+use constant CMP_THRESHOLD		=>    1.00; # do not allow average pixel error to exceed this number of standard deviations
+use constant COLOR_DENSITY_RATIO	=>    0.90; # never reduce color count to less than this amount of the original
+use constant MIN_UNIQUE_COLORS		=> 4096;    # never compress an image with fewer colors; they pixelate
+use constant QUALITY_MAX  		=>  100;    # maximum possible JPEG quality
+use constant QUALITY_MIN  		=>   50;    # minimum quality bound, never go below
+use constant MAX_ITERATIONS		=>    5;    # maximum number of steps
 
-if ($#ARGV < 1)
+if ($#ARGV != 1)
 {
 	print "Usage: $0 <image> <dst>\n";
 	exit 1;
@@ -36,16 +45,29 @@ if (! -f $src)
 my $img = Image::Magick->new();
 $img->Read($src);
 
-my $tmp = search_quality($img);
+printf "Before quality:%u colors:%u size:%.1fKB\n",
+	$img->Get('quality'), unique_colors($img), (-s $src) / 1024.;
+my $QUALITY_MAX = min($img->Get('quality'), QUALITY_MAX);
+my $QUALITY_MIN = max($QUALITY_MAX - MAX_ITERATIONS ** 2, QUALITY_MIN);
+
+my $tmp = search_quality($img, $dst);
 $tmp->Strip(); # strip an image of all profiles and comments
 $tmp->Write($dst);
 
+# never produce a larger image; if our results did, fall back to the original
 if (-s $dst > -s $src)
 {
 	copy($src, $dst) or die $!;
+	$tmp = $img->Clone();
 }
-print_stats();
 
+my $ks = (-s $src) / 1024.;
+my $kd = (-s $dst) / 1024.;
+my $ksave = $ks - $kd;
+my $kpct = $ksave * 100 / $ks;
+
+printf "After quality:%u colors:%u size:%.1fKB saved:(%.1fKB %.1f%%)\n",
+	$tmp->Get('quality'), unique_colors($tmp), $kd, $ksave, $kpct;
 exit;
 
 sub unique_colors
@@ -70,7 +92,7 @@ the visual quality of the original
 =cut
 sub search_quality
 {
-	my ($img) = @_;
+	my ($img, $dst) = @_;
 	my $tmp = $img->Clone();
 
 	if (unique_colors($tmp) < MIN_UNIQUE_COLORS)
@@ -79,23 +101,27 @@ sub search_quality
 	}
 
 	my $original_density = color_density($img);
-	my $qmin =   0;
-	my $qmax = 100;
+	my $qmin = $QUALITY_MIN;
+	my $qmax = $QUALITY_MAX;
 
-	while ($qmax > max(2, $qmin+1))
+	# binary search for lowest quality within given thresholds
+	while ($qmax > $qmin + 1)
 	{
 		my ($q, $diff, $cmpstddev);
 		$q = ($qmax + $qmin) / 2;
 		$tmp = $img->Clone();
 		$tmp->Set(quality => $q);
+		# write image out and read it back in for quality change to take effect
 		$tmp->Write($dst);
 		undef $tmp;
 		$tmp = Image::Magick->new();
 		$tmp->Read($dst);
+		# calculate difference between 'tmp' image and original
 		$diff = $img->Compare(  image => $tmp,
 					metric => 'rmse');
 		$cmpstddev = $diff->Get('error') * 100;
 		my $density_ratio = color_density($tmp) / $original_density;
+		# divide search space in half; which half depending on whether this step passed or not
 		if ($cmpstddev > CMP_THRESHOLD || $density_ratio < COLOR_DENSITY_RATIO)
 		{
 			$qmin = $q;
@@ -106,15 +132,5 @@ sub search_quality
 	}
 	print "\n";
 	return $tmp;
-}
-
-sub print_stats
-{
-	my $k0 = (-s $src) / 1024.;
-	my $k1 = (-s $dst) / 1024.;
-	my $ksave = $k0 - $k1;
-	my $kpct = $ksave * 100 / $k0;
-	printf "Before:%.1fKB After:%.1fKB Saved:%.1fKB(%.1f%%)\n",
-		$k0, $k1, $ksave, $kpct;
 }
 
