165be94e7cd8824822d1089cd4ca08301a2c12e6
Ryan Flynn <parseerror@gmail.com>
2011-06-23 11:38:48 -0400

    remove cmpthreshold as a parameter; our program should JUST WORK™

--- imgmin.pl
+++ imgmin.pl
@@ -5,39 +5,26 @@
 # PerlMagick: http://www.imagemagick.org/script/perl-magick.php
 #
 
+# TODO: consider incorporating: http://en.wikipedia.org/wiki/Color_vision#Physiology_of_color_perception
+
 use strict;
 use Image::Magick;
 use File::Copy qw(copy);
+use List::Util qw(max);
 
 $|++;
 
-=head1
-Color density refers to the ratio of total unique colors in an image
-and its total size (height * width in pixels).
-
-	color_density = width * height / unique_colors
-
-Generally the types of images that compress best have large amounts of
-unique colors which can be reduced without noticably affecting the image
-visibly.
-Some types of images though, like gradients (fades) and line-drawings
-have a small amount of total colors, so reducing the quality at all results
-in awful pixelation.
-In my experience a photo-type image has a color_density in the range of [1,13].
-Gradients otoh have a color_density > 1000.
-This threshold is used to avoid even attempting to optimize gradients and other
-images known to do poorly.
-=cut
-use constant COLOR_DENSITY_THRESHOLD => 20;
+use constant CMP_THRESHOLD		=>    0.90;
+use constant COLOR_DENSITY_RATIO	=>    0.95; 
+use constant MIN_UNIQUE_COLORS		=> 4096; 
 
 if ($#ARGV < 1)
 {
-	print "Usage: $0 <image> <dst> [quality]\n";
+	print "Usage: $0 <image> <dst>\n";
 	exit 1;
 }
 
-my ($src, $dst, $cmpthreshold) = @ARGV;
-$cmpthreshold ||= 1.0;
+my ($src, $dst) = @ARGV;
 
 if (! -f $src)
 {
@@ -48,41 +35,55 @@ if (! -f $src)
 my $img = Image::Magick->new();
 $img->Read($src);
 
-# calculate inverse color density
-my $width = $img->Get('%w');
-my $height = $img->Get('%h');
-my $unique_colors = $img->Get('%k');
-my $density = $width * $height / $unique_colors;
-if ($density < COLOR_DENSITY_THRESHOLD)
-{
-	my $tmp = search_quality($img, $cmpthreshold);
-	$tmp->Strip(); # strip an image of all profiles and comments
-	$tmp->Write($dst);
-}
-else
+my $tmp = search_quality($img);
+$tmp->Strip(); # strip an image of all profiles and comments
+$tmp->Write($dst);
+
+if (-s $dst > -s $src)
 {
 	copy($src, $dst) or die $!;
 }
-
 print_stats();
 
 exit;
 
+sub unique_colors
+{
+	my ($img) = @_;
+	my $unique_colors = $img->Get('%k');
+	return $unique_colors;
+}
+
+sub color_density
+{
+	my ($img) = @_;
+	my $width = $img->Get('%w');
+	my $height = $img->Get('%h');
+	my $density = unique_colors($img) / ($width * $height);
+	return $density;
+}
+
 =head1
-search for the smallest (lowest quality) image whose
-mean pixel error is within cmpthreshold
+search for the smallest (lowest quality) image that retains
+the visual quality of the original
 =cut
 sub search_quality
 {
-	my ($img, $cmpthreshold) = @_;
+	my ($img) = @_;
+	my $tmp = $img->Clone();
 
-	my $tmp;
+	if (unique_colors($tmp) < MIN_UNIQUE_COLORS)
+	{
+		return $tmp;
+	}
+
+	my $original_density = color_density($img);
 	my $qmin =   0;
 	my $qmax = 100;
 
-	while ($qmax > $qmin+1)
+	while ($qmax > max(2, $qmin+1))
 	{
-		my ($q, $diff, $cmppct);
+		my ($q, $diff, $cmpstddev);
 		$q = ($qmax + $qmin) / 2;
 		$tmp = $img->Clone();
 		$tmp->Set(quality => $q);
@@ -92,13 +93,15 @@ sub search_quality
 		$tmp->Read($dst);
 		$diff = $img->Compare(  image => $tmp,
 					metric => 'rmse');
-		$cmppct = $diff->Get('error') * 100;
-		if ($cmppct > $cmpthreshold) {
+		$cmpstddev = $diff->Get('error') * 100;
+		my $density_ratio = color_density($tmp) / $original_density;
+		if ($cmpstddev > CMP_THRESHOLD || $density_ratio < COLOR_DENSITY_RATIO)
+		{
 			$qmin = $q;
 		} else {
 			$qmax = $q;
 		}
-		printf " %.2f@%u", $cmppct, $q;
+		printf " %.2f/%.2f@%u", $cmpstddev, $density_ratio, $q;
 	}
 	print "\n";
 	return $tmp;
