e859ca189ab4556c3606a74cf104c81638fec0a6
Ryan Flynn <parseerror@gmail.com>
2011-07-30 17:33:45 -0400

    add proof-of-concept apache2 mod_ext_filter imgmin wrapper

---
+++ apache/mod_imgmin.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+# ex: set ts=8 noet:
+#
+# Author: Ryan Flynn <parseerror+imgmin@gmail.com>
+# imgmin wrapper for apache OutputFilter
+# imgmin: https://github.com/rflynn/imgmin
+#
+# read image file contents from STDIN
+# if I haven't seen this file before
+#	write it to disk
+#	run imgmin.pl on it
+# write resulting image contents to STDOUT
+
+use strict;
+use warnings;
+
+use Digest::MD5 qw(md5_hex);
+use File::Path qw(make_path);
+
+use constant CACHE_BASEDIR => "/var/imgmin-cache/";
+use constant IMGMIN_BASEDIR => "../";
+
+my $contents = do {
+	# slurp STDIN
+	local $/;
+	<STDIN>;
+};
+
+my $digest = md5_hex($contents);
+
+my $path = $digest;
+$path =~ s/^(..)(..)(..)(.*)$/$1\/$2\/$3\/$4/;
+$path = CACHE_BASEDIR . $path;
+
+if (! -e $path)
+{
+	my ($dir) = ($path =~ m#^(.*/)#);
+	make_path($dir);
+	open(F, ">$path") or die "$path: $!";
+	print F $contents;
+	close F;
+	system((IMGMIN_BASEDIR . "imgmin.pl", $path, $path));
+}
+
+exec "cat $path";
+
