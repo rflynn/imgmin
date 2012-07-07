91edf55bf723a9078edc2ba8acfbf16bdad2fe08
Ryan Flynn <parseerror@gmail.com>
2012-07-07 11:58:04 -0400

    really delete previous images and summarize before/after size

--- test/test.sh
+++ test/test.sh
@@ -13,26 +13,43 @@ filesize()
     echo $(du -b "$1" | cut -f1)
 }
 
-echo "imgmin is designed to reduce filesize while retaining quality."
-echo "At worst, we should fallback to the original file."
-echo "Ensure that we never *increase* filesize."
+pct_less()
+{
+    before=$1
+    after=$2
+    diff=$(($before-$after))
+    echo $((100-(((($before-$diff)*100)/$before))))
+}
 
-for before in $(find ../examples | grep -E "\.(png|jpg)" | grep -v -- "-after")
+echo "Ensure that savings are always [0%,100%)"
+
+rm -f after-*.{jpg,gif,png} 2>/dev/null #clear previous output
+
+beforetotal=0
+aftertotal=0
+for before in $(find ../examples | grep -E "\.(png|gif|jpg)" | grep -v -- "-after" | sort)
 do
     echo "test $before " | tr -d '\n'
     after="after-"$(basename "$before")
-    rm -f "$after" 2>/dev/null #clear previous output
     out=$($IMGMIN "$before" "$after" 2>&1)
-    beforesize=$(filesize "$before")
-    aftersize=$(filesize "$after")
-    if [ $aftersize -gt $beforesize ]
+    beforebytes=$(filesize "$before")
+    afterbytes=$(filesize "$after")
+    totalbefore=$(($totalbefore+$beforebytes))
+    totalafter=$(($totalafter+$afterbytes))
+    if [ $afterbytes -gt $beforebytes ]
     then
         echo "FAIL"
-        echo "before:$beforesize after:$aftersize"
+        echo "before:$beforebytes after:$afterbytes"
         echo "$out"
     else
-        echo "ok"
-        rm -f "$after"
+        pct_saved=$(pct_less $beforebytes $afterbytes)
+        echo "ok ($pct_saved%)"
     fi
 done
+
+total_pct_saved=$(pct_less $totalbefore $totalafter)
+printf "Before: %5.0fk\n" $(($totalbefore/1024))
+printf "After:  %5.0fk (%d%% saved)\n" \
+    $(($totalafter/1024)) \
+    $total_pct_saved
 
